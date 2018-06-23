30e1c470a538fad866c58f1f2a0fae2532e9042d
Yuki <>
2018-06-23 10:10:35 +0900

    PRJ作りました

---
+++ ToDoApp/ToDoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Yuki on 2018/06/23.
+//  Copyright © 2018年 Yuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ToDoApp/ToDoAppTests/ToDoAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  ToDoAppTests.swift
+//  ToDoAppTests
+//
+//  Created by Yuki on 2018/06/23.
+//  Copyright © 2018年 Yuki. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoApp
+
+class ToDoAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
